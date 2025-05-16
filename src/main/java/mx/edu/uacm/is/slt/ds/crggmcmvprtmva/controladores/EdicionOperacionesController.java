94c93a287e95bbac4b434a5d4dfbf7bb259f177e
capriimanuel55 <brandon.martinez@alumnos.uacm.edu.mx>
2025-05-15 21:22:04 -0600

    Primer Commit se agregaron los fxml Panel de control y Edicion de operacion

---
+++ src/main/java/mx/edu/uacm/is/slt/ds/crggmcmvprtmva/controladores/EdicionOperacionesController.java
@@ -0,0 +1,78 @@
+package mx.edu.uacm.is.slt.ds.crggmcmvprtmva.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class EdicionOperacionesController {
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnDetener;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private Button btnPausar;
+
+    @FXML
+    private Button btnRenaudar;
+
+    @FXML
+    private ComboBox<?> cbEstado;
+
+    @FXML
+    private TableColumn<?, ?> colDetalles;
+
+    @FXML
+    private TableColumn<?, ?> colEstado;
+
+    @FXML
+    private TableColumn<?, ?> colTarea;
+
+    @FXML
+    private Label lblNombre;
+
+    @FXML
+    private TableView<?> tvEstados;
+
+    @FXML
+    private TextField txtDescripcion;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    void btnCancelar_OneClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDetener_OneClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGuardar_OneClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPausar_OneClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRenaudar_OneClick(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/mx/edu/uacm/is/slt/ds/crggmcmvprtmva/controladores/PenelControlController.java
@@ -0,0 +1,70 @@
+package mx.edu.uacm.is.slt.ds.crggmcmvprtmva.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class PenelControlController {
+
+    @FXML
+    private Button btnAcerca;
+
+    @FXML
+    private Button btnOperaciones;
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML
+    private Button btnTareas;
+
+    @FXML
+    private TableColumn<?, ?> colDetalles;
+
+    @FXML
+    private TableColumn<?, ?> colEstado;
+
+    @FXML
+    private TableColumn<?, ?> colNombre;
+
+    @FXML
+    private Label lblTitulo;
+
+    @FXML
+    private AnchorPane principal;
+
+    @FXML
+    private TableView<?> tableviewTareas;
+
+    @FXML
+    void btnAcerca_OneClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOperaciones_OneClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSalir_OneClick(ActionEvent event) {
+        System.exit(0);
+
+    }
+
+    @FXML
+    void btnTareas_OneClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selecionar_MauseClicked(MouseEvent event) {
+
+    }
+
+}
