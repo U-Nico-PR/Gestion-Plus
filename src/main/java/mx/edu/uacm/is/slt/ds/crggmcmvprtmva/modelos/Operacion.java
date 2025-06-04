623c0b262e5fe90da8df7b1d33967ebce11b5cf2
U-Nico-PR <ulisesnicolasp_r_@hotmail.com>
2025-06-04 10:06:53 -0600

    Se implementan las clases según se definieron en el documento de propuesta de solución. A partir de aquí cada quien va hacer lo que se le encomendo hacer.

--- src/main/java/mx/edu/uacm/is/slt/ds/crggmcmvprtmva/modelos/Operacion.java
+++ src/main/java/mx/edu/uacm/is/slt/ds/crggmcmvprtmva/modelos/Operacion.java
@@ -1,12 +1,64 @@
 package mx.edu.uacm.is.slt.ds.crggmcmvprtmva.modelos;
 
-public class Operacion {
-    private String identificador;
-    private String nombere;
-    private Estado estado;
-    private Tarea tareas;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Operacion(String nombre) {
-        estado = Estado.NO_EJECUTADA;
+public class Operacion implements IMetodosEstados {
+
+    // Atributos
+    private String nombre;
+    private List<Tarea> tareas;
+    private EnumEstado estado;
+
+    // Constructor
+    public Operacion (String nombre){
+        this.nombre = nombre;
+        tareas = new ArrayList<Tarea>();
+        this.estado = EnumEstado.NO_EJECUTADA;
     }
+
+    // Métodos sobreescritos de la interfaz
+    @Override
+    public void ejecutar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void reanudar() {
+
+    }
+
+    @Override
+    public void detener() {
+
+    }
+
+    @Override
+    public void actualizar(EnumEstado estado) {
+
+    }
+
+    @Override
+    public EnumEstado getEstado() {
+        return null;
+    }
+
+    // Métodos propios de la clase
+    public void agregarTarea(Tarea tarea){
+        tareas.add(tarea);
+    }
+
+    public void eliminarTarea(Tarea tarea){
+        tareas.remove(tarea);
+    }
+
+    public void modificarTarea(Tarea tarea){
+
+    }
+
 }
