f62bb3adc5906e752adea23a1606ce79e64dce68
Juan Francisco <sisigeros@outlook.es>
2025-06-04 18:15:13 -0600

    En esta parte estan los cambios a esas clases e intefaces

--- src/main/java/mx/edu/uacm/is/slt/ds/crggmcmvprtmva/modelos/Operacion.java
+++ src/main/java/mx/edu/uacm/is/slt/ds/crggmcmvprtmva/modelos/Operacion.java
@@ -1,12 +1,91 @@
 package mx.edu.uacm.is.slt.ds.crggmcmvprtmva.modelos;
 
-public class Operacion {
-    private String identificador;
-    private String nombere;
-    private Estado estado;
-    private Tarea tareas;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Operacion implements IMetodosEstados {
+    private String nombre;
+    private List<Tarea> tareas;
+    private EnumEstado estado;
 
     public Operacion(String nombre) {
-        estado = Estado.NO_EJECUTADA;
+        this.nombre = nombre;
+        this.tareas = new ArrayList<>();
+        this.estado = EnumEstado.NO_EJECUTADA;
+    }
+
+    @Override
+    public void ejecutar() {
+        estado = EnumEstado.EJECUTANDO;
+        for (Tarea tarea : tareas) {
+            tarea.ejecutar();
+        }
+        verificarEstado();
+    }
+
+    @Override
+    public void pausar() {
+        if (estado == EnumEstado.EJECUTANDO) {
+            for (Tarea tarea : tareas) {
+                if (tarea.getEstado() == EnumEstado.EJECUTANDO && tarea.isPausable()) {
+                    tarea.pausar();
+                }
+            }
+            estado = EnumEstado.PAUSADA;
+        }
+    }
+
+    @Override
+    public void reanudar() {
+        if (estado == EnumEstado.PAUSADA) {
+            for (Tarea tarea : tareas) {
+                if (tarea.getEstado() == EnumEstado.PAUSADA) {
+                    tarea.reanudar();
+                }
+            }
+            estado = EnumEstado.EJECUTANDO;
+        }
+    }
+
+    @Override
+    public void detener() {
+        estado = EnumEstado.DETENIDA;
+        for (Tarea tarea : tareas) {
+            tarea.detener();
+        }
+    }
+
+    @Override
+    public void actualizar(EnumEstado nuevoEstado) {
+        this.estado = nuevoEstado;
+        verificarEstado();
+    }
+
+    @Override
+    public EnumEstado getEstado() {
+        return estado;
+    }
+
+    public void agregarTarea(Tarea tarea) {
+        tareas.add(tarea);
+    }
+
+    public void eliminarTarea(Tarea tarea) {
+        tareas.remove(tarea);
+    }
+
+    public void modificarOrden(int index, boolean moverArriba) {
+        if (moverArriba && index > 0) {
+            Collections.swap(tareas, index, index - 1);
+        } else if (!moverArriba && index < tareas.size() - 1) {
+            Collections.swap(tareas, index, index + 1);
+        }
+    }
+
+    private void verificarEstado() {
+        if (tareas.stream().allMatch(t -> t.getEstado() == EnumEstado.FINALIZADA)) {
+            estado = EnumEstado.FINALIZADA;
+        }
     }
 }
--- src/main/java/mx/edu/uacm/is/slt/ds/crggmcmvprtmva/modelos/Tarea.java
+++ src/main/java/mx/edu/uacm/is/slt/ds/crggmcmvprtmva/modelos/Tarea.java
@@ -1,10 +1,82 @@
 package mx.edu.uacm.is.slt.ds.crggmcmvprtmva.modelos;
 
-public class Tarea {
+public class Tarea extends Thread implements IMetodosEstados {
     private String nombre;
     private String precondiciones;
     private String postcondiciones;
-    private String intruciones;
-    private String estado;
+    private String instrucciones;
+    private boolean pausable;
+    private boolean tareaInicial;
+    private EnumEstado estado;
+    private boolean running = true;
 
+    public Tarea(String nombre, String precondiciones, String postcondiciones, String instrucciones, boolean pausable, boolean tareaInicial) {
+        this.nombre = nombre;
+        this.precondiciones = precondiciones;
+        this.postcondiciones = postcondiciones;
+        this.instrucciones = instrucciones;
+        this.pausable = pausable;
+        this.tareaInicial = tareaInicial;
+        this.estado = EnumEstado.NO_EJECUTADA;
+    }
+
+    @Override
+    public void run() {
+        ejecutar();
+        while (running) {
+            try {
+                System.out.println("Ejecutando tarea: " + nombre);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Tarea interrumpida: " + nombre);
+                Thread.currentThread().interrupt();
+                running = false;
+            }
+        }
+    }
+
+    @Override
+    public void ejecutar() {
+        actualizar(EnumEstado.EJECUTANDO);
+        new Thread(this).start(); // Inicia un nuevo hilo
+    }
+
+    @Override
+    public void pausar() {
+        if (pausable) {
+            actualizar(EnumEstado.PAUSADA);
+            running = false;
+        }
+    }
+
+    @Override
+    public void reanudar() {
+        if (pausable && estado == EnumEstado.PAUSADA) {
+            running = true;
+            actualizar(EnumEstado.EJECUTANDO);
+            new Thread(this).start();
+        }
+    }
+
+    @Override
+    public void detener() {
+        actualizar(EnumEstado.DETENIDA);
+        running = false;
+        this.interrupt();
+    }
+
+    @Override
+    public void actualizar(EnumEstado estado) {
+        this.estado = estado;
+        System.out.println("Estado actualizado a: " + estado);
+    }
+
+    @Override
+    public EnumEstado getEstado() {
+        return estado;
+    }
+
+    public boolean isPausable() {
+        return pausable;
+    }
 }
