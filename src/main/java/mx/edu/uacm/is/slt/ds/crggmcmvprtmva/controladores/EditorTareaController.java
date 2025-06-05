d11dfc9e6368cfbc377aa8194d89d61b702187b9
Juan Francisco <sisigeros@outlook.es>
2025-06-05 00:35:36 -0600

    Agregar el editor de tareas

---
+++ src/main/java/mx/edu/uacm/is/slt/ds/crggmcmvprtmva/controladores/EditorTareaController.java
@@ -0,0 +1,55 @@
+package mx.edu.uacm.is.slt.ds.crggmcmvprtmva.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class EditorTareaController {
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private Label lblComportamiento;
+
+    @FXML
+    private Label lblModificarPost;
+
+    @FXML
+    private Label lblModificarPre;
+
+    @FXML
+    private Label lblPausable;
+
+    @FXML
+    private Label lblTituloEditorTareas;
+
+    @FXML
+    private RadioButton rdbtnPausable;
+
+    @FXML
+    private TextField txtComportamiento;
+
+    @FXML
+    private TextField txtModPost;
+
+    @FXML
+    private TextField txtModPrecon;
+
+    @FXML
+    void Cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Guardar_OnClick(ActionEvent event) {
+
+    }
+
+}
